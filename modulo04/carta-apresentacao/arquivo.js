721458984ef2e32ece7ff9b95a6f317cd0db226f
Rafael de Jesus <rafael19@outlook.com.br>
2026-09-22 11:48:08 -0300

    adiciona pasta e aquivo

---
+++ modulo04/carta-apresentacao/arquivo.js
@@ -0,0 +1,15 @@
+let nome = "Marina Alves";
+let idade = 22;
+let cidade = "Curitiba";
+let curso = "Trilha JavaScript - LionsDev";
+
+console.log("========================");
+console.log("CARTÃO DE APRESENTAÇÃO");
+console.log("========================");
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade + " anos");
+console.log("Cidade: " + cidade);
+console.log("Curso: " + curso);
+
+console.log("========================");
